4df7f8d199e9623dae978fa09a35f16b7b43dbca
EderLin <ederlin4290@gmail.com>
2025-02-19 00:44:19 +0800

    商品功能 - 實作查詢商品列表（基礎實作）

--- src/main/java/com/eder/springbootmall/dao/ProductDao.java
+++ src/main/java/com/eder/springbootmall/dao/ProductDao.java
@@ -3,8 +3,11 @@ package com.eder.springbootmall.dao;
 import com.eder.springbootmall.dto.ProductReq;
 import com.eder.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
 
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductReq req);
--- src/main/java/com/eder/springbootmall/service/ProductService.java
+++ src/main/java/com/eder/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.eder.springbootmall.service;
 import com.eder.springbootmall.dto.ProductReq;
 import com.eder.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
